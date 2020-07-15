2766354739832fc5766608b3a0e18454b5d3ba12
ITotalJustice <47043333+ITotalJustice@users.noreply.github.com>
2020-07-15 07:35:41 +0100

    Ground now has htibox

--- src/background/Background.gd
+++ src/background/Background.gd
@@ -6,3 +6,8 @@ const SPEED := 100.0
 
 func _physics_process(delta):
 	$ParallaxBackground.scroll_offset.x -= SPEED * delta
+
+
+func _on_Hitbox_body_entered(body):
+	if body.has_method("hit"):
+		body.hit()
